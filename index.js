25349ff3e38ccfafd23ab18d2386b6ed0b36192a
VlSave <vladsava131@yandex.ru>
2018-07-24 12:06:36 +0300

    added express.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import Express from "express";
+import url from 'url';
+import querystring from 'querystring';
+import getURL from './getURL';
+import writeURL from './writeURL';
+
+const app = new Express();
+const port = 5000;
+
+
