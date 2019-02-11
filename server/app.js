8b04796fd7b1d6efa03f67901034461caaeb5e7f
VlSave <vladsava131@yandex.ru>
2019-02-11 03:57:01 +0300

    again

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ const Users = DBConnection.model('Users', usersScheme);
 
 const app = Express();
 
-app.use(Express.static('server/assets'));
+app.use('/static', Express.static(`${__dirname}/assets`));
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use(bodyParser.urlencoded({ extended: false }));
